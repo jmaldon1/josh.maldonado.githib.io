1dc63b77d126f1193175ab2e3fe81d3e7c718ae0
Joshua Maldonado <joshjm9915@gmail.com>
2021-01-30 23:47:53 -0500

    added styling to date seperator

--- src/app/containers/HomePage/components/DateSeperator/index.tsx
+++ src/app/containers/HomePage/components/DateSeperator/index.tsx
@@ -8,7 +8,6 @@ import styled from 'styled-components/macro';
 
 import { Layout } from 'react-grid-layout';
 import Toast from 'react-bootstrap/Toast';
-
 import _ from 'lodash';
 
 interface Props {
@@ -17,8 +16,9 @@ interface Props {
 
 export const DateSeperator = memo(({ layout }: Props) => {
   const date = _.split(layout.i, '_')[0];
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   return (
-    <CenteredToast className="date-seperator">
+    <CenteredToast className="date-seperator" randomColor={randomColor}>
       <Toast.Header closeButton={false}>
         <CenteredDiv>
           <strong className="mr-auto">{date}</strong>
@@ -40,4 +40,6 @@ const CenteredToast = styled(Toast)`
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
+  border-bottom-color: ${props => `#${props.randomColor}`};
 `;
